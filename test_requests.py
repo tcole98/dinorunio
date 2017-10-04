61bc4243e00c0c2c5dd21d68c3d0b2d9135757a6
tcole98 <tristanhcole@gmail.com>
2017-10-04 22:27:18 +1100

    updated db

--- test_requests.py
+++ test_requests.py
@@ -4,8 +4,9 @@ r = requests.post('http://127.0.0.1:5000/create', json={'nickname': 'fdsa', 'sco
 
 r = requests.get('http://127.0.0.1:5000/getdata')
 
+# eb ssh dinorunio-dev
 # /opt/python/bundle/2/app/app.db
-# ssh -i /Users/tristancole/.ssh/aws-eb ec2-user@13.210.14.206 'cat /opt/python/bundle/4/app/app.db' > ~/desktop/app.db
+# ssh -i /Users/tristancole/.ssh/aws-eb ec2-user@13.210.14.206 'cat /opt/python/bundle/10/app/app.db' > ~/desktop/app.db
 # ssh -i /Users/Nick/.ssh/nickpersonal ec2-user@54.206.13.163 ‘cat /var/app/app.db’ > ~/desktop/app.db
 # cat  ~/desktop/app.db | ssh -i /Users/Nick/.ssh/nickpersonal ec2-user@13.55.123.172 ‘sudo cat ->  /var/app/app.db’
 # eb ssh
